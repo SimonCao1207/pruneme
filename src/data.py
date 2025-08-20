82a0e25ea890f893e36f7bb86117826b26229b4a
SimonCao1207 <hainamkaist@gmail.com>
2025-08-20 14:49:18 +0900

    add wikitext dataset

--- src/data.py
+++ src/data.py
@@ -1,6 +1,7 @@
 import torch
 from datasets import load_dataset
 from torch.utils.data import DataLoader, IterableDataset
+from transformers.data.data_collator import DataCollatorForLanguageModeling
 
 from src.config import Config
 
@@ -36,9 +37,50 @@ def get_dataloader(config, collate_fn=collate_fn):
             collate_fn=lambda batch: collate_fn(batch, config),
             num_workers=0,
         )
+    elif config.dataset_name == "wikitext":
+        dataloader = _get_dataloader_wikitext(config.tokenizer, config.batch_size)
     else:
         raise ValueError(f"Unsupported dataset: {config.dataset_name}")
     return dataloader
+
+
+def _get_dataloader_wikitext(tokenizer, batch_size=6, block_size=1024):
+    val_dataset = load_dataset("wikitext", "wikitext-2-raw-v1", split="validation")
+
+    val_dataset = val_dataset.filter(lambda example: len(example["text"]) > 0)
+
+    def tokenize_function(examples):
+        return tokenizer(examples["text"], truncation=False)
+
+    tokenized_dataset = val_dataset.map(tokenize_function, batched=True, remove_columns=["text"])
+
+    def group_texts(examples):
+        # Concatenate all texts.
+        concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+        total_length = len(concatenated_examples[list(examples.keys())[0]])
+        # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
+        # customize this part to your needs.
+        total_length = (total_length // block_size) * block_size
+        # Split by chunks of block_size.
+        result = {
+            k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+            for k, t in concatenated_examples.items()
+        }
+        result["labels"] = result["input_ids"].copy()
+        return result
+
+    lm_dataset = tokenized_dataset.map(
+        group_texts,
+        batched=True,
+    )
+    assert isinstance(lm_dataset, IterableDataset), "lm_dataset should be an IterableDataset"
+
+    print(f"Dataset processed into {len(lm_dataset)} blocks of size {block_size}.")
+
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+    val_loader = torch.utils.data.DataLoader(lm_dataset, batch_size=batch_size, shuffle=False, collate_fn=data_collator)
+    return val_loader
 
 
 if __name__ == "__main__":
